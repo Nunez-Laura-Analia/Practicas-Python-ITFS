e3b4b2e476426e38f07cc49298146ca71d2ec1f4
Laura Analia Nuñez <nunezlauraanalia@gmail.com>
2022-08-29 18:50:10 -0300

    entrega 1

---
+++ main.py
@@ -0,0 +1,52 @@
+# EJERCICIO 1
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+c = int(input("Ingrese el segundo número: "))
+total1 = a + b + c
+print("Resultado: ", total1)
+
+# EJERCICIO 2
+d = float(input("Ingrese el primer número: "))
+e = float(input("Ingrese el segundo número: "))
+total2 = d / e
+print("Resultado:", total2)
+
+# EJERCICIO 3
+f = int(input("Ingrese el primer número: "))
+g = int(input("Ingrese el segundo número: "))
+total3 = f + g
+print("Resultado: ", total3)
+
+# EJERCICIO 4
+h = float(input("Ingrese el primer número: "))
+i = float(input("Ingrese el segundo número: "))
+total4 = h * i
+print("Resultado: ", total4)
+
+# EJERCICIO 5
+j = int(input("Ingrese el primer número: "))
+k = float(input("Ingrese el segundo número: "))
+total5 = k / j
+print("Resultado: ", total5)
+
+# EJERCICIO 6
+l = str(input("Ingrese el primer texto: "))
+m = str(input("Ingrese el segundo texto: "))
+total6 = l + m
+print("Resultado ", total6)
+
+# EJERCICIO 7
+o = int(input("Ingrese su año de nacimiento "))
+total7 = (2022 - o) / 10
+print("Usted vivio ", total7, " decadas")
+
+# EJERCICIO 8
+q = int(input("Ingrese su año de nacimiento "))
+r = int(input("Ingrese el año actual "))
+total8 = (r - q) * 365
+print("Usted vivio ", total8, " dias.")
+
+# EJERCICIO 9
+s = int(input("Ingrese los dias que vivio "))
+total9 = s / 365
+print("Usted vivio ", total9)
